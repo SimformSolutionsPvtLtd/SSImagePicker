bfb23e8390f6fe2fd89a4050cd3fb29e6b5bfc05
soham <soham.s@simformsolutions.com>
2024-01-22 17:54:03 +0530

    - Read me changes

--- buildSrc/src/main/kotlin/ClassPaths.kt
+++
@@ -1,5 +0,0 @@
-object ClassPaths {
-    const val ANDROID_GRADLE = "com.android.tools.build:gradle:${Versions.ANDROID_GRADLE}"
-    const val KOTLIN_GRADLE = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.KOTLIN_GRADLE}"
-    const val BINTRAY = "com.jfrog.bintray.gradle:gradle-bintray-plugin:${Versions.BINTRAY}"
-}
